46615ca7e480fc606ffff8fe8a37bbff8661e015
Yefry Sanchez <yefrymsp@gmail.com>
2025-03-07 23:12:07 -0500

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
+      <Navbar />
+      
       <h1>Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <header className="border p-4">
+      <nav className="flex justify-between items-center">
+        <div>
+          <h1 className="font-bold text-3xl tracking-tighter "> 
+            <span className="text-green-600">Day</span>
+            <span className="text-fuchsia-400">Care</span>
+          </h1>
+        </div>
+        <a href="#" className="inline-block text-white font-bold bg-black px-8 py-3 rounded-full hover:bg-fuchsia-400 transition-colors duration-300">Contact Us</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
