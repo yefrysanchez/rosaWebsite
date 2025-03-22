3fcdc5058c4ce7bfbfafe1c3b3fd0e6817b88d78
Yefry Sanchez <yefrymsp@gmail.com>
2025-03-21 20:42:11 -0400

    Testimonials infinite Scroll

--- src/components/TestimonialSection.tsx
+++ src/components/TestimonialSection.tsx
@@ -1,17 +1,51 @@
 import TestimonialCard from "./TestimonialCard";
 import testimonials from "../data/testimonials";
+import useMeasure from "react-use-measure";
+import { motion, useMotionValue } from "motion/react";
+import { useEffect,  } from "react";
+import { animate } from "motion";
 
 const TestimonialSection = () => {
+  const [ref, { width }] = useMeasure();
+  const xTranslation = useMotionValue(0);
+
+  const finalPosition = -(width + (24 * 10.8)); // Adjust for the total length of all cards and gap
+
+
+  useEffect(() => {
+    const controls = animate(xTranslation, [0, finalPosition], {
+      ease: "linear",
+      duration:  25,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+      onUpdate: (latest) => {
+        if (Number(latest) === finalPosition) {
+          xTranslation.set(0); // Smoothly reset to 0 for infinite scroll
+        }
+      },
+    });
+  
+    return controls.stop;
+  }, [xTranslation, finalPosition]);
+
   return (
-    <div className="max-w-5xl mx-auto lg:mt-12 relative overflow-hidden">
-      <h3></h3>
-      <div className="absolute top-0 left-0 h-full w-20 bg-linear-to-r from-gray-50 to-transparent"></div>
-      <div className="absolute top-0 right-0 h-full w-20 bg-linear-to-r from-transparent to-gray-50"></div>
-      <div className="flex gap-2 mx-auto mt-12">
-        {testimonials.map((t, i) => (
+    <div 
+      className="max-w-5xl mx-auto lg:mt-12 relative overflow-hidden"
+    
+    >
+      {/* Left Gradient Overlay */}
+      <div className="absolute z-10 top-0 left-0 h-full w-20 bg-gradient-to-r from-gray-50 to-transparent"></div>
+
+      {/* Right Gradient Overlay */}
+      <div className="absolute z-10 top-0 right-0 h-full w-20 bg-gradient-to-r from-transparent to-gray-50"></div>
+
+      <motion.div ref={ref} style={{ x: xTranslation }} className="flex w-fit gap-6 mt-12">
+        {/* Testimonials with a sufficient number of duplicates for smooth scrolling */}
+        {[...testimonials, ...testimonials].map((t, i) => (
           <TestimonialCard key={i} testimonial={t.review} name={t.name} />
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
